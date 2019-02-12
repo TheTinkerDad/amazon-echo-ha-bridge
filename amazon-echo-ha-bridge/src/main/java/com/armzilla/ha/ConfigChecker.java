152061389dfbfc36f9b81d8274832413ad499606
damaniac82 <laszlo.merczel@gmail.com>
2019-02-12 18:05:25 +0100

    Added environment specific configuration

--- amazon-echo-ha-bridge/src/main/java/com/armzilla/ha/ConfigChecker.java
+++ amazon-echo-ha-bridge/src/main/java/com/armzilla/ha/ConfigChecker.java
@@ -2,7 +2,6 @@ package com.armzilla.ha;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
