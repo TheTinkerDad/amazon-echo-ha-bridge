81edc50a0923b73c69a366a3e923fddb0e3b6a70
damaniac82 <laszlo.merczel@gmail.com>
2019-02-13 10:47:13 +0100

    Changed the environment specific configuration to use the standard Spring Boot mechanism.

--- amazon-echo-ha-bridge/src/main/java/com/armzilla/ha/SpringBootEntry.java
+++ amazon-echo-ha-bridge/src/main/java/com/armzilla/ha/SpringBootEntry.java
@@ -3,16 +3,11 @@ package com.armzilla.ha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan
-@PropertySource({
-	"classpath:config.common.properties",
-	"classpath:config.${env}.properties"
-})
 public class SpringBootEntry {
 
     public static void main(String[] args) {
